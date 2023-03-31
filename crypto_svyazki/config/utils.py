cffa5bf9d6b7b4bf4ffbabae1d558eddbc221670
Nuu8rs <nyrsik20172017@gmail.com>
2023-03-31 17:44:54 +0300

    123456789

--- crypto_svyazki/config/utils.py
+++ crypto_svyazki/config/utils.py
@@ -1,2 +1,15 @@
 api_key = "nTfCKRwFem4nLTeDisMrZelsxrgKeQYyXlP0rKeaiY05giJOyT5zTiEYWiqtEEA7"
 api_secret = "NLTGPyxp2dR3wA1zxqXcv3BZKuQRSmjqKukxws7uFrwsl6Be8v090rBdtHFi3ngJ"
+
+bybit_api_key = "506L5hWn46It9tlvy5"
+bybit_api_secret = "Js7JkDCDZsU0bF9MuN7XyNzOWkZCn2kk9rtB"
+
+OKX_key = ""
+OKX_api_secter = ""
+
+prices = {
+   "binance": {},
+   "okx": {},
+   "huobi": {},
+   "bybit": {}   
+}
--- crypto_svyazki/crypto_obrabotka/crypto_bot.py
+++ crypto_svyazki/crypto_obrabotka/crypto_bot.py
@@ -1,15 +1,72 @@
 import sys
 sys.path.insert(0, '../CRYPTO_SVYZKI')
 
-from binance.client import Client
+from config.utils import api_key,api_secret , bybit_api_key , bybit_api_secret , OKX_key , OKX_api_secter
 
-from config.utils import api_key,api_secret
-# Замените YOUR_API_KEY и YOUR_SECRET_KEY своими ключами API
+from config.utils import prices
+
+from datetime import datetime
+import time
+import requests
+import hashlib
+import json
+import asyncio
+#===========================================
+from binance.client import Client    #Бинанс
+from pybit import HTTP               #ByBit
+
 client = Client(api_key=api_key, api_secret=api_secret)
+http = HTTP()
 
-# Получить все цены на токены на Binance
-tickers = client.get_all_tickers()
+#===========================================
 
-# Распечатать список цен на токены
-for ticker in tickers:
-    print(ticker)
+
+
+
+
+async def get_all_token_binance():
+    global client
+
+    tickers = client.get_ticker()
+    usdt_tokens = [ticker for ticker in tickers if ticker['symbol'].endswith('USDT')]
+
+    for token in usdt_tokens:
+        prices["binance"][token["symbol"].replace("USDT","")] = (float(token["askPrice"]), float(token["bidPrice"]))
+async def get_all_token_bybit():
+    global HTTP
+    results = http.best_bid_ask_price()
+    for x in results.get("result"):
+        if "USDT" in x["symbol"]:
+            prices["bybit"][x["symbol"].replace("USDT","")] = (float(x["askPrice"]), float(x["bidPrice"]))
+
+
+async def get_all_token_OKX():
+    params = {
+        "instType": "SPOT",
+        "quoteCcy": "USDT",
+        "api_key": OKX_key
+    }
+    response = requests.get("https://www.okex.com/api/v5/market/tickers", params=params)
+
+    data = response.json()
+    for ticker in data["data"]:
+        symbol = ticker["instId"]
+        if symbol.split("-")[1] == "USDT":
+            prices["okx"][symbol.replace("-USDT","")] = (float(ticker["askPx"]), float(ticker["bidPx"]))
+
+async def get_all_token_huobi():
+    huobi_ticker_url = "https://api.huobi.pro/market/tickers"
+    huobi_tickers = requests.get(huobi_ticker_url).json()["data"]
+    for t in huobi_tickers:
+        symbol = t["symbol"]
+        if "usdt" in symbol:
+            prices["huobi"][symbol.replace("usdt","")] = (float(t["ask"]), float(t["bid"]))
+
+
+
+
+async def waiting():
+    await asyncio.wait([get_all_token_binance(), get_all_token_bybit(), get_all_token_OKX() , get_all_token_huobi()])
+    print(prices)
+
+asyncio.run(waiting())
